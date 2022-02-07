d252de621c1d3cd12bc19f9d83df5e0a8e14a7f0
andrefpoliveira <andre_pinto_oliveira@outlook.pt>
2022-02-07 09:09:18 +0000

    [ENH] Changed color and body

--- server.py
+++ server.py
@@ -24,10 +24,10 @@ def handle_info():
     print("INFO")
     return {
         "apiversion": "1",
-        "author": "",  # TODO: Your Battlesnake Username
-        "color": "#888888",  # TODO: Personalize
-        "head": "default",  # TODO: Personalize
-        "tail": "default",  # TODO: Personalize
+        "author": "andrefpoliveira",
+        "color": "#79eb8c",
+        "head": "bendr",
+        "tail": "freckled",
     }
 
 
